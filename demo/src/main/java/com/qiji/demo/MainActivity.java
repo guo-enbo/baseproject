c2651c39c56b7f357480340ff733ce4e8d0b2ea0
geb1024 <25416133399@qq.com>
2017-11-15 10:20:39 +0800

    Initial commit

---
+++ demo/src/main/java/com/qiji/demo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.qiji.demo;
+
+
+import android.os.Bundle;
+
+import com.guoenbo.library.ui.activity.base.BaseBussActivity;
+
+public class MainActivity extends BaseBussActivity {
+
+    @Override
+    protected void setLayout(Bundle savedInstanceState) {
+        super.setLayout(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+    }
+
+    @Override
+    protected void BindEvent() {
+        super.BindEvent();
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+}
